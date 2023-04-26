de8829b30eb013fbcdb70d3d8f1027734ba4b30e
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-26 22:00:15 +0700

    open txt files: numbers.txt file (read), even.txt file (append), odd.txt file (append)

--- Maincode.py
+++ Maincode.py
@@ -5,6 +5,8 @@
 #define a function
 def read_append_file():
     #open files named: numbers.txt file (read), even.txt file (append), odd.txt file (append)   
+    with open("integers.txt") as source_file, open("double.txt", "a") as squared_nums, open("triple.txt", "a") as cubed_nums:    
+
         #read the numbers.txt file line by line
         #check each lines of numbers.txt
             #IF the line has even number,
